606aa4676d2889259409677fc4b043eb8dd7dccb
Di Wang <wangdicoder@gmail.com>
2020-10-31 23:25:21 +1100

    chore: update webpack.config.js

--- site/config/webpack.config.js
+++ site/config/webpack.config.js
@@ -3,69 +3,70 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: path.resolve(__dirname, '../src/index.js'),
-	output: {
-		filename: 'bundle.[hash].js',
-		path: path.resolve(__dirname, '../build'),
-		publicPath: '/',
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: 'babel-loader',
-			},
-			{
-				test: /\.tsx?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'ts-loader',
-					options: {
-						configFile: path.resolve(__dirname, 'tsconfig.json'),
-					},
-				},
-			},
-			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 10240,
-							name: path.join('img/[name].[hash:7].[ext]'),
-							esModule: false,
-						},
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: path.resolve(__dirname, '../public/index.html'),
-			inject: true,
-		}),
-		new webpack.HotModuleReplacementPlugin(),
-	],
-	devtool: 'source-map',
-	devServer: {
-		host: 'localhost',
-		port: 3000,
-		historyApiFallback: true,
-		overlay: {
-			errors: true,
-		},
-		inline: true,
-		hot: true,
-	},
-}
+  mode: 'development',
+  entry: path.resolve(__dirname, '../src/index.js'),
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, '../build'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: path.resolve(__dirname, 'tsconfig.json'),
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              name: path.join('img/[name].[hash:7].[ext]'),
+              esModule: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, '../public/index.html'),
+      inject: true,
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devtool: 'source-map',
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    historyApiFallback: true,
+    overlay: {
+      errors: true,
+    },
+    inline: true,
+    hot: true,
+  },
+};
--- site/src/index.js
+++ site/src/index.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import CheckoutButton from '../../src';
 
 const App = () => {
-  return (<CheckoutButton countryCode="AU" token="1231">Shop now with</CheckoutButton>);
+  return (
+    <CheckoutButton countryCode="CA" token="1231">
+      Shop now with
+    </CheckoutButton>
+  );
 };
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
