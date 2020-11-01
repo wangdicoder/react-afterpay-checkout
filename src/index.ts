426fdd697919a90b78ceac3520057947b4845f39
Di Wang <wangdicoder@gmail.com>
2020-11-01 20:51:08 +1100

    fix: window undefined issue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { Button as default } from './button';
 
-if(!('Afterpay' in window)) {
+if (typeof window !== 'undefined' && !('Afterpay' in window)) {
   const element = document.createElement('script');
   element.src = 'https://portal.afterpay.com/afterpay.js';
   document.head.appendChild(element);
