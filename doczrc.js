45e2476ebe5f440d810d0e49e2d36cbac17dc12d
Di Wang <wangdicoder@gmail.com>
2020-11-01 22:05:43 +1100

    docs: update docz configs

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,8 @@
 export default {
+  base: '/react-afterpay-checkout/',
+  dest: '/site',
+  files: 'docs/*.{md,markdown,mdx}',
   typescript: true,
   port: 8080,
-  menu: ['Home', 'Example'],
+  menu: ['Get started', 'Example'],
 };
