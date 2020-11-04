f6f42f4ec06764d873404e4a01a95d2923d91c8b
Di Wang <wangdicoder@gmail.com>
2020-11-04 23:15:08 +1100

    fix: remove useless code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
 export { Button as default } from './button';
-
-if (typeof window !== 'undefined' && !('Afterpay' in window)) {
-  const element = document.createElement('script');
-  element.src = 'https://portal.afterpay.com/afterpay.js';
-  document.head.appendChild(element);
-}
