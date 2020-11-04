242f05bbfa73183539a0c42b3498cb5910a97574
Di Wang <wangdicoder@gmail.com>
2020-11-04 23:14:10 +1100

    chore: update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
 const path = require('path');
 
 module.exports = {
-	mode: 'production',
-	entry: './src/index.ts',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.min.js',
-		publicPath: './dist/',
-		libraryTarget: 'commonjs2',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 10240,
-							name: path.join('img/[name].[hash:7].[ext]'),
-							esModule: false,
-						},
-					},
-				],
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	externals: {
-		react: 'react',
-	},
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.min.js',
+    publicPath: './dist/',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              name: path.join('img/[name].[hash:7].[ext]'),
+              esModule: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  externals: {
+    react: 'react',
+  },
 };
